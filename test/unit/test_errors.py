5474d27c2cece2a8666f72b6f85d57495fef9a1a
thomasATbayer <thomas.wolf@bayer.com>
2026-08-03 11:06:42 +0000

    fix: error check with mother-ml

--- test/unit/test_errors.py
+++ test/unit/test_errors.py
@@ -7,8 +7,9 @@ def test_extras_dependency_import_error():
 
     assert isinstance(error, ExtrasDependencyImportError)
     assert str(nested_error) in str(error)
-    assert "pip install 'mother[example]'" in str(error)
-    assert "uv add 'mother[example]'" in str(error)
+    assert "pip install 'mother-ml[example]'" in str(error)
+    assert "uv add mother-ml --extra example" in str(error)
+    assert "uv sync --extra example" in str(error)
 
 
 def test_configuration_error():
